16968abe70f500c8f47cc0a0e2bccf8e6eabf62c
gruvw <gruvw.dev@gmail.com>
2024-08-14 13:14:38 +0200

    feat: load yaml documents from Dart, fixes #7

--- lib/views/routes/overview.dart
+++ lib/views/routes/overview.dart
@@ -1,10 +1,18 @@
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
+import "package:yaml/yaml.dart";
 
 class OverviewPage extends StatelessWidget {
   const OverviewPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // final data = rootBundle.loadString("assets/content.yaml");
+
+    rootBundle
+        .loadString("assets/content.yaml")
+        .then((data) => print(loadYamlDocuments(data)));
+
     return Scaffold(
       appBar: AppBar(title: const Text("StoryText")),
       body: const Center(child: Text("Hello World")),
